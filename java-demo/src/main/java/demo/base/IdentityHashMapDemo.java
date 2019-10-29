4993f5095b5a0e93486ee3facf147e21d7613df3
zhouchao <chao.zhou@raykite.com>
2019-10-29 11:11:19 +0800

    增加IdentityHashMapDemo 修改多值map的例子

---
+++ java-demo/src/main/java/demo/base/IdentityHashMapDemo.java
@@ -0,0 +1,28 @@
+package demo.base;
+
+import org.springframework.util.LinkedMultiValueMap;
+
+import java.util.Arrays;
+import java.util.IdentityHashMap;
+import java.util.Map;
+
+/**
+ * @Author zhouchao
+ * @Date 2019/10/29 11:08
+ * @Description
+ **/
+
+public class IdentityHashMapDemo {
+    public static void main(String[] args) {
+        Map<String, Object> map = new IdentityHashMap<>();
+
+        String oldString = "123";
+        String newString = new String("123");
+
+        map.put(oldString, "oldString");
+        map.put(newString,"newString");
+
+        System.out.println(map.get(oldString));
+        System.out.println(map.get(newString));
+    }
+}
