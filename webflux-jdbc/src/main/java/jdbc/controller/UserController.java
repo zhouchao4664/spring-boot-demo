5f54f8ea6a5e3ad13bd290d685542952b4b95dd5
zhouchao4664 <zhouchao19890214@163.com>
2020-01-01 22:46:33 +0800

    controller执行变为异步

--- webflux-jdbc/src/main/java/jdbc/controller/UserController.java
+++ webflux-jdbc/src/main/java/jdbc/controller/UserController.java
@@ -6,12 +6,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * 2018/8/26
  * zhouchao
  */
 @RestController
 public class UserController {
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(5);
 
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -21,6 +26,10 @@ public class UserController {
 
     @PostMapping("/web/mvc/user/save")
     public boolean save(@RequestBody User user){
+        executorService.submit(()->{
+            userRepository.save(user);
+        });
+
         return userRepository.save(user);
     }
 }
