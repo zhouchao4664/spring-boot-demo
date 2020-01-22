b70220fbb8db9bef85d2f029eaf76bdb329f7d61
zhouchao <chao.zhou@raykite.com>
2020-01-22 11:08:51 +0800

    修改controller注解为RestController

--- scheduler/src/main/java/scheduler/controller/QuartzController.java
+++ scheduler/src/main/java/scheduler/controller/QuartzController.java
@@ -1,18 +1,17 @@
 package scheduler.controller;
 
 import org.quartz.*;
-import org.quartz.impl.StdScheduler;
 import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author zhouchao
  * @Date 2020/1/22 10:50
  * @Description
  **/
-@Controller
+@RestController
 public class QuartzController {
 
     @Autowired
@@ -28,7 +27,8 @@ public class QuartzController {
         scheduler.scheduleJob(jobDetail, trigger);
         scheduler.start();
 
-        return "success";
+        String result = "success";
+        return result;
     }
 
 
