f5bd1c2ff8e74d57210f6542e3e4f857d8e8d3a5
zhouchao <chao.zhou@raykite.com>
2020-10-22 19:28:32 +0800

    增加MyWebSecurityConfigurer.java

---
+++ security/src/main/java/com/zhouchao/security/config/MyWebSecurityConfigurer.java
@@ -0,0 +1,13 @@
+package com.zhouchao.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Author zhouchao
+ * @Date 2020/10/22 18:29
+ * @Description
+ **/
+@Configuration
+public class MyWebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+}
