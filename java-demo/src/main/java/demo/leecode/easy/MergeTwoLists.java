2615aef03cd35a35a5d22c14848d1de699fbabf8
zhouchao <zhouchao19890214@163.com>
2020-11-24 00:23:27 +0800

    增加算法类，合并两个有序链表

---
+++ java-demo/src/main/java/demo/leecode/easy/MergeTwoLists.java
@@ -0,0 +1,44 @@
+package demo.leecode.easy;
+
+public class MergeTwoLists {
+
+    //Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode prev = new ListNode(0);
+        ListNode head = prev;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = l1 == null ? l2 : l1;
+        return head.next;
+    }
+
+
+}
