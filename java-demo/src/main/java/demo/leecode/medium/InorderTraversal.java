7c68e08d0439e385b8ba1a7f9aa6f0fde423e688
zhouchao <chao.zhou@raykite.com>
2020-11-27 23:53:26 +0800

    增加算法类，前序遍历

---
+++ java-demo/src/main/java/demo/leecode/medium/InorderTraversal.java
@@ -0,0 +1,35 @@
+package demo.leecode.medium;
+
+import java.util.List;
+
+/**
+ * @Author zhouchao
+ * @Date 2020/11/27 22:16
+ * @Description https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+ **/
+
+public class InorderTraversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        return null;
+    }
+}
