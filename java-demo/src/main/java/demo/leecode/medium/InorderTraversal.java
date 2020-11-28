6a49fd36c9dac192dbbe208415d824dc1c9eb865
zhouchao <zhouchao19890214@163.com>
2020-11-28 15:49:41 +0800

    增加算法类，二叉树的中序遍历

--- java-demo/src/main/java/demo/leecode/medium/InorderTraversal.java
+++ java-demo/src/main/java/demo/leecode/medium/InorderTraversal.java
@@ -1,5 +1,6 @@
 package demo.leecode.medium;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -29,7 +30,11 @@ public class InorderTraversal {
     }
 
     public List<Integer> inorderTraversal(TreeNode root) {
-
-        return null;
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        result.addAll(inorderTraversal(root.left));
+        result.add(root.val);
+        result.addAll(inorderTraversal(root.right));
+        return result;
     }
 }
