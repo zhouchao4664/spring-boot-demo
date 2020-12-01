17a08b01de2a79011c14b4b5f135142435eddb5c
zhouchao <chao.zhou@raykite.com>
2020-12-01 21:00:45 +0800

    优化爬楼梯

--- java-demo/src/main/java/demo/leecode/easy/ClimbingStairs.java
+++ java-demo/src/main/java/demo/leecode/easy/ClimbingStairs.java
@@ -8,14 +8,19 @@ package demo.leecode.easy;
 
 public class ClimbingStairs {
     public int climbStairs(int n) {
-        if (n < 3) return n;
-        int result = 0;
-        int f1 = 1, f2 = 2;
-        for (int i = 3; i <= n; i++) {
-            result = f1 + f2;
-            f1 = f2;
-            f2 = result;
+        if(n<=3) return n;
+
+        int f1 = 1;
+        int f2 = 2;
+        for(int i =3;i<=n;i++){
+            f2 += f1;
+            f1 = f2-f1;
         }
-        return result;
+        return f2;
+    }
+
+    public static void main(String[] args) {
+        int n = 7;
+        new ClimbingStairs().climbStairs(n);
     }
 }
