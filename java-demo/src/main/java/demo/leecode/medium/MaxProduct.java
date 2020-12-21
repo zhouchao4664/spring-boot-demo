a9b2f395f819a8f77ee7de4dd38f8248d22007ba
zhouchao <chao.zhou@raykite.com>
2020-12-21 19:35:57 +0800

    增加算法类，152. 乘积最大子数组

--- java-demo/src/main/java/demo/leecode/medium/MaxProduct.java
+++ java-demo/src/main/java/demo/leecode/medium/MaxProduct.java
@@ -8,6 +8,24 @@ package demo.leecode.medium;
 
 public class MaxProduct {
     public int maxProduct(int[] nums) {
-        return 0;
+        int max = nums[0], imax = 1, imin = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                int tmp = imax;
+                imax = imin;
+                imin = tmp;
+            }
+            imax = Math.max(imax * nums[i], nums[i]);
+            imin = Math.min(imin * nums[i], nums[i]);
+
+            max = Math.max(max, imax);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, -2, 4, 8};
+        int result = new MaxProduct().maxProduct(nums);
+        System.out.println(result);
     }
 }
