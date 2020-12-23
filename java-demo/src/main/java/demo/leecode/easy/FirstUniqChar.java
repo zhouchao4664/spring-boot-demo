9d409cfcd05c33de15016ed3ba2c50daca90d424
zhouchao <chao.zhou@raykite.com>
2020-12-23 17:56:49 +0800

    增加算法类,387. 字符串中的第一个唯一字符

--- java-demo/src/main/java/demo/leecode/easy/FirstUniqChar.java
+++ java-demo/src/main/java/demo/leecode/easy/FirstUniqChar.java
@@ -10,16 +10,16 @@ public class FirstUniqChar {
     public int firstUniqChar(String s) {
         //只循环了26次
         int result = s.length();
-        for (char c = 'a'; c <= 'z' ; c++) {
+        for (char c = 'a'; c <= 'z'; c++) {
             int firstIndex = s.indexOf(c);
             int lastIndex = s.lastIndexOf(c);
 
             //如果相等表示不重复
-            if(lastIndex == firstIndex && firstIndex != -1 ){
-                result = Math.min(firstIndex,result);
+            if (lastIndex == firstIndex && firstIndex != -1) {
+                result = Math.min(firstIndex, result);
             }
         }
-        if(result != s.length()){
+        if (result != s.length()) {
             return result;
         }
         return -1;
