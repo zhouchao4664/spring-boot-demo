06ce2227c6f88f8b9a6fc9971066246c85d6543e
zhouchao <chao.zhou@raykite.com>
2021-01-12 02:37:32 +0800

    增加mybatis配置

---
+++ security/src/main/java/com/zhouchao/security/config/MybatisPlusConfig.java
@@ -0,0 +1,20 @@
+package com.zhouchao.security.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author zhouchao
+ * @Date 2021/1/12 1:14
+ * @Description
+ **/
+
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
