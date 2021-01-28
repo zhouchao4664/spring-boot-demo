e7e8f7fabf4f546cb74dfd2645fc63d275cd3fe1
zhouchao <chao.zhou@raykite.com>
2021-01-28 19:29:17 +0800

    增加算法，146. LRU 缓存机制

--- java-demo/src/main/java/demo/leecode/medium/LRUCache.java
+++ java-demo/src/main/java/demo/leecode/medium/LRUCache.java
@@ -1,6 +1,7 @@
 package demo.leecode.medium;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -9,116 +10,25 @@ import java.util.Map;
  * @Description
  **/
 
-public class LRUCache {
-
-    private int capacity = 1;
-    private int size = 0;
-
-    private Map<Integer, Node> map = new HashMap<>();
-
-    Node firstNode = null;
-    Node lastNode = null;
+public class LRUCache extends LinkedHashMap<Integer, Integer> {
+    private int capacity;
 
     public LRUCache(int capacity) {
+        super(capacity, 0.75F, true);
         this.capacity = capacity;
     }
 
     public int get(int key) {
-        if (!map.containsKey(key)) return -1;
-
-        Node n = map.get(key);
-
-        if (n == firstNode) {
-            return n.value;
-        } else {
-            Node nextNode = n.next;
-            Node prevNode = n.prev;
-            if (nextNode != null) nextNode.prev = prevNode;
-            if (prevNode != null) prevNode.next = nextNode;
-        }
-
-        linkFirst(n);
-
-        return n.value;
+        return super.getOrDefault(key, -1);
     }
 
+    // 这个可不写
     public void put(int key, int value) {
-        Node newNode = null;
-
-        if (map.containsKey(key)) {
-
-            Node n = map.get(key);
-            n.value = value;
-
-            Node nextNode = n.next;
-            Node prevNode = n.prev;
-            if (nextNode != null) nextNode.prev = prevNode;
-            if (prevNode != null) prevNode.next = nextNode;
-
-            n.prev = null;
-            n.next = null;
-
-            newNode = n;
-        } else {
-            newNode = new Node(null, value, null);
-
-            linkFirst(newNode);
-
-            size++;
-        }
-
-        map.put(key, newNode);
-        checkCapacity();
+        super.put(key, value);
     }
 
-    private void checkCapacity(){
-        // 如果到达上限，去掉尾部节点
-        if (size > capacity) {
-            Node lastPrev = lastNode.prev;
-            Node last = lastNode;
-            last.prev = null;
-            lastPrev.next = null;
-            lastNode = lastPrev;
-
-            map.remove(last);
-            size--;
-        }
-    }
-
-    private void linkFirst(Node node) {
-        Node f = firstNode;
-        firstNode = node;
-        if (f == null) {
-            lastNode = node;
-        } else {
-            node.prev = null;
-            node.next = f;
-            f.prev = node;
-        }
-    }
-
-    private static class Node {
-        int value;
-        Node next;
-        Node prev;
-
-        Node(Node prev, int value, Node next) {
-            this.value = value;
-            this.next = next;
-            this.prev = prev;
-        }
-    }
-
-    public static void main(String[] args) {
-        LRUCache lruCache = new LRUCache(2);
-        lruCache.put(1,1);
-        lruCache.put(2,2);
-        lruCache.get(1);
-        lruCache.put(3,3);
-        lruCache.get(2);
-        lruCache.put(4,4);
-        lruCache.get(1);
-        lruCache.get(3);
-        lruCache.get(4);
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
     }
 }
