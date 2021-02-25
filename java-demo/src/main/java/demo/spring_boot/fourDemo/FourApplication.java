69ae17637bee2802090bbfcaefc8326ccbb413aa
zhouchao <chao.zhou@raykite.com>
2021-02-25 19:09:35 +0800

    删除自动生成id的模块

--- java-demo/src/main/java/demo/spring_boot/fourDemo/FourApplication.java
+++ java-demo/src/main/java/demo/spring_boot/fourDemo/FourApplication.java
@@ -1,4 +1,4 @@
-package demo.spring_boot.FourDemo;
+package demo.spring_boot.fourDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
