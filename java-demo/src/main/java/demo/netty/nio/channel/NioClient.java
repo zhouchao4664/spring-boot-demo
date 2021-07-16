17e2298502ad09af386e51db2229c8d3c7b97521
zhouchao <zhouchao19890214@163.com>
2021-07-16 20:43:26 +0800

    增加nio的server例子

--- java-demo/src/main/java/demo/netty/nio/channel/NioClient.java
+++ java-demo/src/main/java/demo/netty/nio/channel/NioClient.java
@@ -1,9 +1,30 @@
 package demo.netty.nio.channel;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
+
 /**
  * @Author: zhouchao
  * @Date: 2021/07/13 14:59
  * @Description:
  */
 public class NioClient {
+    public static void main(String[] args) throws IOException {
+        //1. 打开通道
+        SocketChannel socketChannel = SocketChannel.open();
+        //2. 设置连接IP和端口号
+        socketChannel.connect(new InetSocketAddress("127.0.0.1", 9999));
+        //3. 写出数据
+        socketChannel.write(ByteBuffer.wrap("老板还钱".getBytes(StandardCharsets.UTF_8)));
+        //4. 读取服务器写回的数据
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        int read = socketChannel.read(byteBuffer);
+        System.out.println("服务端消息:" + new String(byteBuffer.array(), 0, read, StandardCharsets.UTF_8));
+        //5. 释放资源
+        socketChannel.close();
+    }
+
 }
--- java-demo/src/main/java/demo/netty/nio/channel/NioServer.java
+++ java-demo/src/main/java/demo/netty/nio/channel/NioServer.java
@@ -2,7 +2,10 @@ package demo.netty.nio.channel;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @Author: zhouchao
@@ -10,10 +13,29 @@ import java.nio.channels.ServerSocketChannel;
  * @Description:
  */
 public class NioServer {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-        serverSocketChannel.bind(new InetSocketAddress(8000));
+        serverSocketChannel.bind(new InetSocketAddress(9999));
         serverSocketChannel.configureBlocking(false);
-
+        System.out.println("服务端启动成功");
+        while (true) {
+            // 检查是否有客户端连接
+            SocketChannel socketChannel = serverSocketChannel.accept();
+            if (socketChannel == null) {
+                System.out.println("没有客户端连接...我去做别的事情");
+                Thread.sleep(2000);
+                continue;
+            }
+            // 获取客户端传过来的数据，并把数据放到缓冲区里
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+            //返回值
+            //正数: 表示本地读到有效字节数
+            //0: 表示本次没有读到数据
+            //-1: 表示读到末尾
+            int read = socketChannel.read(byteBuffer);
+            System.out.println("客户端消息：" + new String(byteBuffer.array(), 0, read, StandardCharsets.UTF_8));
+            socketChannel.write(ByteBuffer.wrap("没钱".getBytes(StandardCharsets.UTF_8)));
+            serverSocketChannel.close();
+        }
     }
 }
