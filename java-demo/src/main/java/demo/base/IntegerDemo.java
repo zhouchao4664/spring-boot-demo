c91830dc59b453e1bfe9813aba71729b0a91e946
zhouchao4664 <zhouchao19890214>
2022-06-25 14:05:36 +0800

    增加int常量池的范围

--- java-demo/src/main/java/demo/base/IntegerDemo.java
+++ java-demo/src/main/java/demo/base/IntegerDemo.java
@@ -1,44 +1,20 @@
 package demo.base;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import org.apache.commons.compress.utils.Lists;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-
 /**
  * @Author zhouchao
  * @Date 2020/9/27 11:24
- * @Description
+ * @Description int常量池中初始化-128~127的范围
  **/
 
 public class IntegerDemo {
 
     public static void main(String[] args) {
-        Integer index = 1;
-        List<TestClass> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            index += 1;
-            TestClass testClass = new TestClass(index);
-            list.add(testClass);
-        }
-
-        System.out.println(list);
+        Integer i1 = 1;
+        Integer i2 = 1;
+        System.out.println(i1 == i2);
+        Integer j1 = 1000;
+        Integer j2 = 1000;
+        System.out.println(j1 == j2);
     }
 
-    @AllArgsConstructor
-    @Data
-    private static class TestClass {
-        private Integer age;
-
-        @Override
-        public String toString() {
-            return "TestClass{" +
-                    "age=" + age +
-                    '}';
-        }
-    }
 }
