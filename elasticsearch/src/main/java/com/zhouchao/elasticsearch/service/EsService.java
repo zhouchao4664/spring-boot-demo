cc67def6fa3ce28a43d19af991da9b6e1b3f340f
周超 <zhouchao@xianniu.cn>
2022-12-13 19:30:32 +0800

    es模块增加第二种创建索引的方式

--- elasticsearch/src/main/java/com/zhouchao/elasticsearch/service/EsService.java
+++ elasticsearch/src/main/java/com/zhouchao/elasticsearch/service/EsService.java
@@ -7,6 +7,7 @@ import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -55,17 +56,34 @@ public class EsService {
     public void testCreateIndex() throws IOException {
         CreateIndexRequest createIndexRequest = new CreateIndexRequest("elasticsearch_test");
 
-        XContentBuilder builder = XContentFactory.jsonBuilder()
-                .startObject()
-                .field("properties")
-                .startObject()
-                .field("description").startObject().field("type", "text").field("analyzer", "ik_max_word").endObject()
-                .field("name").startObject().field("type", "keyword").endObject()
-                .field("pic").startObject().field("type", "text").field("index", false).endObject()
-                .field("studymodel").startObject().field("type", "keyword").endObject()
-                .endObject()
-                .endObject();
-        createIndexRequest.mapping(builder);
+//        XContentBuilder builder = XContentFactory.jsonBuilder()
+//                .startObject()
+//                .field("properties")
+//                .startObject()
+//                .field("description").startObject().field("type", "text").field("analyzer", "ik_max_word").endObject()
+//                .field("name").startObject().field("type", "keyword").endObject()
+//                .field("pic").startObject().field("type", "text").field("index", false).endObject()
+//                .field("studymodel").startObject().field("type", "keyword").endObject()
+//                .endObject()
+//                .endObject();
+        createIndexRequest.mapping("{\n" +
+                "            \"properties\":{\n" +
+                "                \"description\":{\n" +
+                "                    \"type\":\"text\",\n" +
+                "                    \"analyzer\":\"ik_max_word\"\n" +
+                "                },\n" +
+                "                \"name\":{\n" +
+                "                    \"type\": \"keyword\"\n" +
+                "                },\n" +
+                "                \"pic\":{\n" +
+                "                    \"type\": \"text\",\n" +
+                "                    \"index\": false\n" +
+                "                },\n" +
+                "                \"studymodel\":{\n" +
+                "                    \"type\":\"keyword\"\n" +
+                "                }\n" +
+                "            }\n" +
+                "        }", XContentType.JSON);
 
         IndicesClient indicesClient = restHighLevelClient.indices();
         CreateIndexResponse createIndexResponse = indicesClient.create(createIndexRequest, RequestOptions.DEFAULT);
