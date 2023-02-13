d6744081f507508610618b67767597cf0b3af579
周超 <zhouchao@xianniu.cn>
2023-02-13 11:55:49 +0800

    修改注释

--- java-demo/src/main/java/demo/thread/LockChangeHashCodeDemo.java
+++ java-demo/src/main/java/demo/thread/LockChangeHashCodeDemo.java
@@ -8,7 +8,7 @@ import org.openjdk.jol.info.ClassLayout;
  * @Description:
  * 对于偏向锁，在线程获取偏向锁时，会用Thread ID和epoch值覆盖identity hash code所在的位置。
  * 如果一个对象的hashCode()方法已经被调用过一次之后，这个对象还能被设置偏向锁么？
- * 答案是不能。因为如果可以的化，那Mark Word中的identity hash code必然会被偏向线程Id给覆盖，
+ * 答案是不能。因为如果可以的话，那Mark Word中的identity hash code必然会被偏向线程Id给覆盖，
  * 这就会造成同一个对象前后两次调用hashCode()方法得到的结果不一致。
  */
 public class LockChangeHashCodeDemo {
@@ -20,7 +20,7 @@ public class LockChangeHashCodeDemo {
         System.out.println("对象头是：" + ClassLayout.parseInstance(lock).toPrintable());
         synchronized (lock){
             System.out.println("对象头是：" + ClassLayout.parseInstance(lock).toPrintable());
-            System.out.println("加锁前的hashCode是：" + System.identityHashCode(lock));
+            System.out.println("加锁后的hashCode是：" + System.identityHashCode(lock));
             System.out.println("对象头是：" + ClassLayout.parseInstance(lock).toPrintable());
         }
     }
