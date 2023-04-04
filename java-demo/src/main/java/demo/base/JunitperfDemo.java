f4e9867c1c2c403d45d8b2d336fae3a1fafb7628
周超 <zhouchao@xianniu.cn>
2023-04-05 04:14:11 +0800

    增加junitperf

---
+++ java-demo/src/main/java/demo/base/JunitperfDemo.java
@@ -0,0 +1,17 @@
+package demo.base;
+
+import com.github.houbb.junitperf.core.annotation.JunitPerfConfig;
+
+/**
+ * @Author zhouchao
+ * @Description TODO
+ * @Date 2023/4/5 2:27
+ */
+public class JunitperfDemo {
+
+    @JunitPerfConfig(duration = 1000)
+    public void test(){
+
+    }
+
+}
