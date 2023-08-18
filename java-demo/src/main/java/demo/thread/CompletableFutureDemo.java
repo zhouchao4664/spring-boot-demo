c2cffc76a8298f0032348ec5ab47282d5f2e1645
周超 <zhouchao@xianniu.cn>
2023-08-18 15:14:27 +0800

    增加completablefuture的demo

---
+++ java-demo/src/main/java/demo/thread/CompletableFutureDemo.java
@@ -0,0 +1,33 @@
+package demo.thread;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+
+/**
+ * @Author zhouchao
+ * @Description 测试CompletableFuture
+ * @Date 2023/8/18 13:36
+ */
+public class CompletableFutureDemo {
+
+    public static ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(0,
+            10,
+            30,
+            TimeUnit.SECONDS,
+            new LinkedBlockingQueue<Runnable>(10),
+            Executors.defaultThreadFactory(),
+            new ThreadPoolExecutor.AbortPolicy());
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(()->{
+            System.out.println("处理任务");
+            return "success";
+        },threadPoolExecutor);
+
+        String s = completableFuture.get();
+        System.out.println(s);
+    }
+
+}
